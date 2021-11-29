39bf918a9a6c5b4a2130e132d6dc65992d46cd9e
MIKELL <86540880+MAYKELL07@users.noreply.github.com>
2021-11-29 19:30:03 +0700

    update clear

--- plugins/_clear.js
+++ plugins/_clear.js
@@ -1,24 +1,16 @@
 let handler = m => m
 
 handler.before = async function (m) {
-
-    if (!global.db.data.settings.clear) return // autoclear aktif?{
-        if (new Date() * 1 - global.db.data.settings.cleartime < 3600000) {
-            let chats = this.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
-            grup = []
-            for (let id of chats) {
-                if (id.endsWith('g.us')) {
-                    grup.push(id)
-                } else {
-                    this.modifyChat(id, 'delete').catch(_ => _)
-                }
-            }
-            for (let i = 0; i < 1; i++) {
-                await this.modifyChat(grup[i], 'clear', {
-                    includeStarred: false
-                }).catch(_ => _)
-            }
+    let chat = db.data.chats[m.chat]
+    if (chat.clearTime != 0 && chat.clear) {
+        if (new Date() * 1 >= chat.clearTime) {
+            await this.modifyChat(m.chat, 'clear', {
+                includeStarred: false
+            }).catch(console.log)
+            chat.clearTime = new Date() * 1 + 60000 * 1
+            console.log(`jid: ${m.chat} | ${this.getName(m.chat)}\nall messages have been cleared`)
         }
-
+    }
 }
+
 module.exports = handler
