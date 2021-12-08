93a366ba2b731ce51fdef5a80f37e8d8b49047f7
maykell07 <ferrelymichaellie@gmail.com>
2021-12-09 06:11:13 +0700

    Update Sewabot. js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,4 +1,4 @@
-let handler = m => m.reply('*OPEN JASA SEWA BOT*\n\nSewa 7 Hari / Grup : 5k\nSewa 30 Hari / Grup : 15k\nTrial 5 Hari : Ketik *.Trial*\n*Payment :*\n*_Dana_*\nMinat?PC\nWa.me/6282324892737')
+let handler = m => m.reply('*OPEN JASA SEWA BOT*\n\nSewa 7 Hari / Grup : 5k\nSewa 30 Hari / Grup : 15k\n\n*Trial 5 Hari : Ketik .Trial <linkgroup>*\n\n*Payment :*\n*_Dana_*\nMinat?PC\nWa.me/6282324892737')
 
 handler.help = ['sewabot']
 handler.tags = ['about']
