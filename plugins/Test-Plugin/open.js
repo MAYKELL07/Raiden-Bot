e279204daa718da435b7d3d178226d6b90a4e30c
MIKELL <86540880+MAYKELL07@users.noreply.github.com>
2021-12-08 01:33:20 +0000

    update test open.js

--- plugins/Test-Plugin/open.js
+++ plugins/Test-Plugin/open.js
@@ -8,25 +8,32 @@ let handler = async (m, { conn, command, args, text, usedPrefix, DevMode }) => {
     let rare = ["fish"];
     let epic = ["banknote"];
     let legendary = ["moracrystal"];
+    let randomnumCommon = `${Math.floor(Math.random() * 11 + 1 * amount)}`.trim()
+    let randomnumUnommon = `${Math.floor(Math.random() * 8 * amount)}`.trim() 
+    let randomnumRare = `${Math.floor(Math.random() * 5 * amount)}`.trim()
+    let randomnumEpic = `${Math.floor(Math.random() * 5 - 2 * amount)}`.trim()
+    let randomnumLegendary = `${Math.floor(Math.random() * 11 - 9 * amount)}`.trim()
     let randCommon = common[Math.floor(Math.random() * common.length)];
     let randUncommon = colors[Math.floor(Math.random() * colors.length)];
     let randRare = colors[Math.floor(Math.random() * colors.length)];
     let randEpic = colors[Math.floor(Math.random() * colors.length)];
     let randLegendary = colors[Math.floor(Math.random() * colors.length)];
     let user = global.db.data.users[m.sender]
+    if (randomnumLegendary < 0) randomnumLegendary = 0; 
+    if (randomnumEpic < 0) randomnumEpic = 0;  
     try {
         switch (type) {
             case 'common':
-                if (user.common >= 1) {
-                    user.common -= 1
-                    user.money += `${Math.floor(Math.random() * 10)}`.trim()
-                    user.exp +=  1
+                if (user.common >= amount) {
+                    user.common -= amount
+                    user.money += `${Math.floor(Math.random() * 10 * amount)}`.trim()
+                    user.exp += `${Math.floor(Math.random() * 10 * amount)}`.trim()
                     if (!user.rpg[randCommon]) { //random common item
                         user.rpg[randCommon] = 0
-                        user.rpg[randCommon] += 1}
+                        user.rpg[randCommon] += randomnumCommon}
                     if (!user.rpg[randUncommon]) { //random uncommon item
                         user.rpg[randUncommon] = 0
-                        user.rpg[randUncommon] += 1}
+                        user.rpg[randUncommon] += randomnumUnommon}
                 }
             break      
         }
