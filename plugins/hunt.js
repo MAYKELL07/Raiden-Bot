6d3adffa3fe746f242a2de3c58514381d77dca47
MIKELL <ferrelymichaellie@gmail.com>
2021-12-12 09:16:23 +0700

    Update Hunt.js

--- plugins/hunt.js
+++ plugins/hunt.js
@@ -3,7 +3,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 
-	let monsters = JSON.parse(fs.readFileSync('./data/rpg/monster.json'))
+	let monsters = JSON.parse(fs.readFileSync('./data/rpg/monster.json')).monsters
 	let player = global.db.data.users[m.sender]
 	let pname = conn.getName(m.sender)
 
@@ -18,14 +18,27 @@ let handler = async (m, { conn, text }) => {
 	let area_monsters = monsters.filter(monster => { return monster.area === areaPlayer })
 	let monster = area_monsters[Math.floor(Math.random() * area_monsters.length)]
 	let monsterName = monster.name.toUpperCase()
+	let randomizer = `${Math.floor(Math.random() * 101)}`.trim()
     
-
-	if (new Date -  global.db.data.users[m.sender].lasthunt > 120000) {
+	if (player.money > 0) {
 		let sum = 10 * areaPlayer - 59
 		let dmg = (player.sword  * 5 + player.armor * 5 - sum)
 		dmg = dmg < 0 ? Math.abs(dmg) : 0
 		let coins = areaPlayer * 50
 		let exp = areaPlayer * 20
+        let monsterdrop = monster.drop
+		let monsterdroped = `dropped nothing`
+
+		if (randomizer <= monster.droprate) {
+		//mosnter drop items
+		player.rpg[monsterdrop]
+		if (!player.rpg[monsterdrop]) player.rpg[monsterdrop] = 0
+		player.rpg[monsterdrop] += 1
+		monsterdroped = `dropped ${monsterdrop}`
+		} else {
+			monsterdroped = `dropped nothing`
+		}
+
 
 		player.healt -= dmg
 		player.lasthunt = new Date * 1 // waktu hunt 2menit
@@ -49,16 +62,17 @@ let handler = async (m, { conn, text }) => {
 			contextInfo: {
 			externalAdReply: {
 			title: monsterName,
-			body: `wibu`,
+			body: monsterdroped,
 			thumbnail: await (await fetch(url)).buffer() ,
 			sourceUrl: 'http://raiden-bot.ga/'}}})
 	} else throw `Tunggu *${cd1}:${cd2}* Untuk Berburu Lagi`
 }
 
-handler.help = ['hunt']
+handler.help = ['thunt']
 handler.tags = ['rpg']
-handler.command = /^hunt/i
+handler.command = /^thunt/i
 
+handler.owner = true
 handler.disabled = false
 
 handler.fail = null
--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-let handler = m => m.reply('*OPEN JASA SEWA BOT*\n\nSewa 7 hari / Grup : 5k\nSewa 30 hari / Grup : 15k\n\nFree Trial 5 Hari Ketik .Trial <linkgroup>\n*Payment :*\n*_Dana_*\nMinat?PC\nWa.me/6289602213818')
-=======
-let handler = m => m.reply('*OPEN JASA SEWA BOT*\n\nSewa 7 Hari / Grup : 5k\nSewa 30 Hari / Grup : 15k\n\n*Trial 5 Hari : Ketik .Trial <linkgroup>*\n\n*Payment :*\n*_Dana_*\nMinat?PC\nWa.me/6282324892737')
->>>>>>> 93a366ba2b731ce51fdef5a80f37e8d8b49047f7
+let handler = m => m.reply('*OPEN JASA SEWA BOT*\n\nSewa 7 hari / Grup : 5k\nSewa 30 hari / Grup : 15k\n\n*Free Trial 5 Hari Ketik .Trial <linkgroup>*\n*Payment :*\n*_Dana_*\nMinat?PC\nWa.me/6289602213818')
 
 handler.help = ['sewabot']
 handler.tags = ['about']
